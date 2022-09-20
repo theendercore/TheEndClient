118c162926f9a83e1c0fd6fd71d457637275336b
theendercore <theendercore@gmail.com>
2022-09-20 15:28:40 +0300

    added to do

--- ECClompiler.py
+++ ECClompiler.py
@@ -1,5 +1,6 @@
+import os
 
+output_path = "/home/ender/.local/share/atlauncher/instances/TheEnd/"
+input_path = "/media/Data/Game_L/PolyMC/instances/TheEndClient/.minecraft/"
 
-
-
-
+os.system('cp -R ' + input_path + ' ./mods')
