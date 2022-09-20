edaf8aa48b4255e6491270d1c44e79dcd052fe1b
theendercore <theendercore@gmail.com>
2022-09-20 18:32:34 +0300

    :)

--- ECClompiler.py
+++ ECClompiler.py
@@ -7,27 +7,26 @@ def log(s):
 
 input_path = "/media/Data/Game_L/PolyMC/instances/TheEndClient/.minecraft/"
 output_path = "/home/ender/.local/share/atlauncher/instances/TheEnd/"
+tmp_path = "./tmp"
 
-# os.system('cp -R ' + input_path + ' ./mods')
-os.system('cp ./perm/servers.dat '+output_path)
+# Create tmp folder
+os.system('mkdir ' + tmp_path)
+
+# Copy the server data
+os.system('cp ./perm/servers.dat ' + tmp_path)
+
+# Copy the mods folder and deletes pre-selected and .disabled files
+tmp_path += "/mods"
+os.system('cp -R ' + input_path + 'mods ' + tmp_path)
+os.system('rm -R ' + tmp_path + '/.index ' + tmp_path + '/libJNativeHook.x86_64.so')
+for root, dirs, files in os.walk(tmp_path):
+    for filename in files:
+        if filename.endswith(".disabled"):
+            log(filename)
+            os.system('rm ' + tmp_path + '/' + filename)
+tmp_path = "./tmp"
 
 
-
-
-
-# in_opt = open(input_path + "options.txt", "r")
-# in_test_opt = in_opt.read().split('\n')
-# exist_opt = open("./perm/options.txt", "r")
-# in_exist_opt = exist_opt.read().split()
-#
-# for in_opt in in_test_opt:
-#     for exist_opt in in_exist_opt:
-#         if in_opt == exist_opt:
-#             # log(in_test_opt)
-#             in_exist_opt.remove(exist_opt)
-#             in_test_opt.remove(in_opt)
-#         # else:
-#             # log(exist_opt + " | " + in_opt)
-#
-# # # log(in_test_opt)
-# log(in_exist_opt)
+# Delete the tmp
+x = input("Delte the tmp")
+os.system('rm -r ' + tmp_path)
