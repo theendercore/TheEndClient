381345be130dd7f7da33517b70cf381dee64d4c6
theendercore <theendercore@gmail.com>
2022-09-20 19:48:23 +0300

    resourcepacks done

--- ECClompiler.py
+++ ECClompiler.py
@@ -5,8 +5,8 @@ def log(s):
     print(s)
 
 
-input_path = "/media/Data/Game_L/PolyMC/instances/TheEndClient/.minecraft/"
-output_path = "/home/ender/.local/share/atlauncher/instances/TheEnd/"
+input_path = "/media/Data/Game_L/PolyMC/instances/TheEndClient/.minecraft"
+output_path = "/home/ender/.local/share/atlauncher/instances/TheEnd"
 tmp_path = "./tmp"
 
 # Create tmp folder
@@ -16,21 +16,28 @@ os.system('mkdir ' + tmp_path)
 os.system('cp ./perm/servers.dat ' + tmp_path)
 
 # Copy the mods folder and deletes pre-selected and .disabled files
-tmp_path += "/mods"
-os.system('cp -R ' + input_path + 'mods ' + tmp_path)
-os.system('rm -R ' + tmp_path + '/.index ' +
-          tmp_path + '/libJNativeHook.x86_64.so')
-for root, dirs, files in os.walk(tmp_path):
-    for filename in files:
-        if filename.endswith(".disabled"):
-            log(filename)
-            os.system('rm ' + tmp_path + '/' + filename)
-tmp_path = "./tmp"
+# tmp_path += "/mods"
+# os.system('cp -R ' + input_path + 'mods ' + tmp_path)
+# os.system('rm -R ' + tmp_path + '/.index ' +
+#           tmp_path + '/libJNativeHook.x86_64.so')
+# for root, dirs, files in os.walk(tmp_path):
+#     for filename in files:
+#         if filename.endswith(".disabled"):
+#             log(filename)
+#             os.system('rm ' + tmp_path + '/' + filename)
+# tmp_path = "./tmp"
 
 # Copy the configs folder and delte not need ones
 tmp_path += "/resourcepacks"
-os.system('cp -R ' + input_path + 'resourcepacks ' + tmp_path)
-# os.system('rm -R -- '+tmp_path+"/")
+os.system('mkdir '+tmp_path)
+resourcepacks = ["§cTea", "baguette", "Ender's-Tweaks", "Original Green"]
+for root, dirs, files in os.walk(input_path+"/resourcepacks"):
+    for rec in resourcepacks:
+        for file in files:
+            if file.startswith(rec):
+                os.system('cp -R \"' + input_path +"/resourcepacks/"+file+"\" "+ tmp_path)
+                log('cp -R \"' + input_path +"/resourcepacks/"+file+"\" "+ tmp_path)
+                log(file+" | " + rec)
 
 tmp_path = "./tmp"
 
--- Todo.c
+++ Todo.c
@@ -2,7 +2,7 @@
 // - copy over sever.dat
 - copy config [delete hydra ones]
 // - delete and then copy over the mods [make sure to delete disabled ones and the extra files]
-- copy over resourcepacks [myb generate things for modrith downloads]
+// - copy over resourcepacks [myb generate things for modrith downloads]
 
 - copy over world map [Multiplayer_wafflessmp.apexmc.co]
 - copy over basic waypoints [ones that do not expose things]
